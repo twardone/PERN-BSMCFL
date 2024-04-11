7619ae6b895fae3e8326469374c576890f0c0762
twardone <terry.ward@ymail.com>
2024-04-10 20:18:27 -0400

    created Dark Theme toggle

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { FaAlignLeft } from 'react-icons/fa'
 import Logo from './Logo'
 import LogoutContainer from './LogoutContainer'
 import { useDashboardContext } from '../pages/DashboardLayout'
+import ThemeToggle from './ThemeToggle'
 
 const Navbar = () => {
   const { toggleSidebar } = useDashboardContext()
@@ -16,8 +17,10 @@ const Navbar = () => {
           <Logo />
           <h4 className='logo-text'>dashboard</h4>
         </div>
-        <div className='btn-container'>toggle/logout</div>
-        <LogoutContainer />
+        <div className='btn-container'>
+          <ThemeToggle />
+          <LogoutContainer />
+        </div>
       </div>
     </Wrapper>
   )
